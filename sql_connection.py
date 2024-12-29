fd5ebfe54ab9f4d4feb811321e93adc5eb617590
Luis Velasquez <luisfernandov1502@gmail.com>
2024-12-28 23:48:44 -0500

    Create and configure the SQLConnection class
    This class will ensure that the SQL connection is handled properly, which means that the connection shall close once it is not needed anymore; preventing data-leaks

---
+++ sql_connection.py
@@ -0,0 +1,42 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+
+# Gather and organize environment variables
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+
+class SQLConnection:
+    def __init__(self):
+        self.connection = None
+        self.cursor = None
+
+    def __enter__(self):
+        self.connection = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        self.cursor = self.connection.cursor()
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.connection:
+            self.connection.close()
+        if self.cursor:
+            self.cursor.close()
+
+
+with SQLConnection() as sql:
+    sql.cursor.execute("SELECT * FROM person")
+    print(sql.cursor.fetchall())
