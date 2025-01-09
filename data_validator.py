ca9f102553d221d737653003310bbfba5e3e8147
Luis Velasquez <luisfernandov1502@gmail.com>
2025-01-09 13:58:30 -0500

    Started working in the new_assignment_data validator function

--- data_validator.py
+++ data_validator.py
@@ -1,5 +1,18 @@
 import metadata
 
 
-def updated_assignment_data(assignment_data: dict):
-    pass
+def new_assignment_data(assignment_data: dict):
+    for field, value in metadata.ASSIGNMENT.items():
+        print(field)
+        if metadata.ASSIGNMENT[field]['required'] and field in assignment_data.keys():
+            continue
+
+        return False
+
+
+data = {
+    "assignment_id": 2,
+    "student_id": 1
+}
+
+print(new_assignment_data(data))
