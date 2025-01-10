00ff042d7a7858f15a4e292e71670639a7c896dc
Luis Velasquez <luisfernandov1502@gmail.com>
2025-01-10 00:20:04 -0500

    Updated the new_assignment_data function to avoid redundancy

--- data_validator.py
+++ data_validator.py
@@ -2,16 +2,17 @@ import metadata
 
 
 def new_assignment_data(assignment_data: dict):
-    for field, value in metadata.ASSIGNMENT.items():
-        print(field)
-        if metadata.ASSIGNMENT[field]['required'] and field in assignment_data.keys():
+    for field, properties in metadata.ASSIGNMENT.items():
+        # Checks for required fields in assignment_data
+        if properties['required'] and field in assignment_data.keys():
             continue
-
         return False
+
+    return True
 
 
 data = {
-    "assignment_id": 2,
+    "assignment_id": 87,
     "student_id": 1
 }
 
