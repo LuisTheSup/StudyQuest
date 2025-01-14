5a6f2c3658077a6987936e34668228417deb60df
Luis Velasquez <luisfernandov1502@gmail.com>
2025-01-14 13:12:54 -0500

    updated the new_assignment_data function to process Required fields

--- data_validator.py
+++ data_validator.py
@@ -1,19 +1,26 @@
 import metadata
+from exceptions import MissingFieldsError
 
 
 def new_assignment_data(assignment_data: dict):
+    missing_fields = []
     for field, properties in metadata.ASSIGNMENT.items():
-        # Checks for required fields in assignment_data
-        if properties['required'] and field in assignment_data.keys():
-            continue
-        return False
+        # Checks for missing fields in assignment_data
+        if properties['required'] and field not in assignment_data.keys():
+            missing_fields.append(field)
 
+    if missing_fields:
+        raise MissingFieldsError(missing_fields)
     return True
 
-
+# TEST DATA
 data = {
     "assignment_id": 87,
-    "student_id": 1
+    "student_id": 1,
+    # "name": "study guide",
+    "category": "formative",
+    # "total_points": 40,
+    # "weighting": 1.0,
 }
 
 print(new_assignment_data(data))
