2fae37851f374edb2b4388a25ba390c438002695
Luis Velasquez <luisfernandov1502@gmail.com>
2025-01-14 13:10:24 -0500

    Added functions to retrieve specific columns with in the students and assignments tables

--- db.py
+++ db.py
@@ -71,6 +71,23 @@ def read_assignments():
         return postgres.cursor.fetchall()
 
 
+def read_assignments_column( column: str, *, assignment_id: int = None):
+    """
+        Reads a specific assignment's column from the table if an assignment id is provided
+        otherwise it will read the same column from every assignment in the table
+        :param column: Column to read
+        :param assignment_id: The ID of the assignment to read - DEFAULTS to None
+        :return: list[tuple]
+        """
+    with SQLConnection() as postgres:
+        if assignment_id is None:
+            query = f"SELECT {column} FROM assignments"
+        else:
+            query = f"SELECT {column} FROM assignments WHERE assignment_id = %s"
+        postgres.cursor.execute(query, (assignment_id,))
+        return postgres.cursor.fetchall()
+
+
 # Students related queries
 def add_student(student_data: dict):
     with SQLConnection as postgres:
@@ -147,3 +164,20 @@ def student_exists(student_id: int):
         query = "SELECT EXISTS(SELECT 1 FROM students WHERE student_id = %s);"
         postgres.cursor.execute(query, (student_id,))
         return postgres.cursor.fetchone()[0]
+
+
+def read_students_column(column: str, *, student_id: int = None):
+    """
+    Reads a specific student's column from the table if a student id is provided
+    otherwise it will read the same column from every student in the table
+    :param column: Column to read
+    :param student_id: The ID of the student to read - DEFAULTS to None
+    :return: list[tuple]
+    """
+    with SQLConnection() as postgres:
+        if student_id is None:
+            query = f"SELECT {column} FROM students"
+        else:
+            query = f"SELECT {column} FROM students WHERE student_id = %s"
+        postgres.cursor.execute(query, (student_id,))
+        return postgres.cursor.fetchall()
