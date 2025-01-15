dca1f57d08727d39d7a6e0d6ca0f6b0d49053b06
Luis Velasquez <luisfernandov1502@gmail.com>
2025-01-15 12:58:31 -0500

    Added new custom exception - InvalidFieldsError. which will be raised when an invalid field is found

--- exceptions.py
+++ exceptions.py
@@ -1,4 +1,10 @@
 class MissingFieldsError(Exception):
-    def __init__(self, missing_fields):
+    def __init__(self, missing_fields: list[str]):
         super().__init__(f'Missing Required Fields - {', '.join(missing_fields)}')
         self.missing_fields = missing_fields
+
+
+class InvalidFieldsError(Exception):
+    def __init__(self, invalid_fields: list[str]):
+        super().__init__(f"Invalid Fields -> {', '.join(invalid_fields)}")
+        self.invalid_fields = invalid_fields
